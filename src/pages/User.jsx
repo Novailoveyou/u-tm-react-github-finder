8b620dd5c34f04202f95765736306fdafdee8349
novailoveyou <novailoveyoux@gmail.com>
2022-08-08 00:37:04 +0300

    made User structure better

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -32,6 +32,29 @@ const User = () => {
     hireable
   } = user
 
+  const testimonials = [
+    {
+      Icon: FaUsers,
+      label: 'Followers',
+      value: followers
+    },
+    {
+      Icon: FaUserFriends,
+      label: 'Following',
+      value: following
+    },
+    {
+      Icon: FaCodepen,
+      label: 'Public Repos',
+      value: public_repos
+    },
+    {
+      Icon: FaStore,
+      label: 'Public Gists',
+      value: public_gists
+    }
+  ]
+
   if (loading) return <Spinner />
 
   return (
@@ -113,42 +136,17 @@ const User = () => {
         </div>
 
         <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaUsers className='text-3xl md:text-5xl' />
+          {testimonials.map(({ Icon, label, value }) => (
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <Icon className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>{label}</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {value}
+              </div>
             </div>
-            <div className='stat-title pr-5'>Followers</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {followers}
-            </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaUserFriends className='text-3xl md:text-5xl' />
-            </div>
-            <div className='stat-title pr-5'>Following</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {following}
-            </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaCodepen className='text-3xl md:text-5xl' />
-            </div>
-            <div className='stat-title pr-5'>Public Repos</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {public_repos}
-            </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaStore className='text-3xl md:text-5xl' />
-            </div>
-            <div className='stat-title pr-5'>Public Gists</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {public_gists}
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
